8f98f1a08a543e434e85c640a14da7bd64f96faa
klipski <lipskikamil94@gmail.com>
2018-02-18 14:04:36 +0100

    add python version of script

---
+++ python/ultrasonic_distance.py
@@ -0,0 +1,36 @@
+#-*- coding: utf-8 -*-
+
+import RPi.GPIO as GPIO
+import time
+
+# set pin numbering mode to BOARD
+# and disable warnings
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+# receiver pin
+ECHO_PIN = 31
+# sender pin
+TRIGGER_PIN = 29
+
+# set trigger pin as output
+GPIO.setup(TRIGGER_PIN, GPIO.OUT)
+# set echo pin as input
+GPIO.setup(ECHO_PIN, GPIO.IN)
+
+GPIO.output(TRIGGER_PIN, True)
+time.sleep(0.00001)
+GPIO.output(TRIGGER_PIN, False)
+
+# wait for high state
+while GPIO.input(ECHO_PIN) == 0:
+    start = time.time()
+# sensor response
+while GPIO.input(ECHO_PIN) == 1:
+    end = time.time()
+
+# calculate result
+elapsed = end - start
+measure = elapsed * 17150
+
+print (measure)
